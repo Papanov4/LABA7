1a331eb26eb115e37256804810d46afe637cd71d
Papanov4 <platunov04@bk.ru>
2023-06-10 23:43:05 +0300

    проверили работу 2

--- Weather.cpp
+++ Weather.cpp
@@ -1,12 +1,24 @@
 #include "Weather.h"
+Weather::Weather(string city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds) {
 
-Weather::Weather(std::string city, double lon, double lat, double temperature, std::string weather, double windSpeed, int clouds)
+    this->city = city;
+    this->lon = lon;
+    this->lat = lat;
+    this->temperature = temperature;
+    this->weather = weather;
+    this->windSpeed = windSpeed;
+    this->clouds = clouds;
+
+}
+
+void Weather::getData() {
+
+    cout << city << ": lon(" << lon << "), lat(" << lat << "), t(" << temperature << "), weather(" << weather << "), wind speed(" << windSpeed << "), clouds(" << clouds << ")\n";
+
+}
+
+std::ostream& operator<<(std::ostream& out, Weather weatherrr)
 {
-	this->city = city;
-	this->lon = lon;
-	this->lat = lat;
-	this->temperature = temperature;
-	this->weather = weather;
-	this->windSpeed = windSpeed;
-	this->clouds = clouds;
+    out << weatherrr.city << ": \nlon(" << weatherrr.lon << "); \nlat(" << weatherrr.lat << "); \nt(" << weatherrr.temperature << "); \nweather(" << weatherrr.weather << "); \nwind speed(" << weatherrr.windSpeed << "); \nclouds(" << weatherrr.clouds << ")\n";
+    return out;
 }
--- WeatherService1.cpp
+++ WeatherService1.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#include "Weather.h"
+#include "XmlService.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+XmlService xs;
+Weather w = xs.getWeather("weather.xml");
+cout << w;
+}
+
