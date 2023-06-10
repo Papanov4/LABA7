f638b37c745f270e037318a1a5ec9661a8324720
Papanov4 <platunov04@bk.ru>
2023-06-10 23:23:11 +0300

    проверили работу

---
+++ JsonService.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Service.h"
+#include "json.hpp"
+
+using nlohmann::json;
+
+class JsonService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~JsonService() {};
+};
--- WeatherService1.cpp
+++ WeatherService1.cpp
@@ -1,1 +1,9 @@
-﻿
+﻿#include "JsonService.h"
+#include "Service.h"
+#include "json.hpp"
+
+int main()
+{
+	JsonService js;
+	Weather w = js.getWeather("weather.json");	
+}
